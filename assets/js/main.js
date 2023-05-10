020f405571d34b76ac5019a8812c7942e2e3f163
vulopez <111790061+vulopez@users.noreply.github.com>
2023-05-09 22:01:38 -0500

    Add exception's function for strict encryption mode
    Check if the input meets the criteria in config.

---
+++ assets/js/main.js
@@ -0,0 +1,64 @@
+const input = document.querySelector("#text"); // <textarea> selector
+const output = document.querySelector(".output-msg"); // response <p> selector
+
+const hash = {
+    alura5: {
+        a: "ai",
+        e: "enter",
+        i: "imes",
+        o: "ober",
+        u: "ufat",
+    },
+};
+
+let config = {
+    method: hash.alura5,
+    strict: true,
+    exceptions: {
+        uppercase: /(?=.*[A-Z])/,
+        // lowercase: /(?=.*[a-z])/,
+        // space: /\s/,
+        // punctuation: /(?=.*[,.;:])/,
+        digits: /(?=.*\d)/,
+        accents: /(?=.*[À-ÿ])/,
+        // TODO: Delete this unnecesary `\`
+        symbols: /(?=.*[¡!¿?@#$%^&*)(=<>{\[\]}'"|~`+_-])/,
+    },
+    hash: hash.alura5,
+};
+
+function tryEncrypt() {
+    const text = input.value;
+
+    if (contains(text, config.exceptions)) return cannotEncrypt();
+
+    return encrypt();
+}
+
+/**
+ * Check if some RegExp exceptions are in a string
+ *
+ * @param {string} str     The text input
+ * @param {object} pattern Patterns that can't be in `str`
+ *
+ * @returns {boolean} `true` if some matchs else `false`
+ */
+function contains(str, pattern) {
+    for (const regex in pattern) {
+        if (pattern[regex].test(str)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+// TODO: Make this function work
+function encrypt() {
+    console.log("I can encrypt it! 😁");
+}
+
+// TODO: Make this function work
+function cannotEncrypt() {
+    console.log("I can't encrypt that! 😞");
+}
